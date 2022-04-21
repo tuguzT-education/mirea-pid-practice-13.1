09e4c14c92c2b250256e37a8164836bc04a2e094
tuguzT <timurka.tugushev@gmail.com>
2022-04-21 22:09:50 +0300

    Complete tasks

--- app/src/main/java/io/github/tuguzt/getusdrate/RateCheckService.kt
+++ app/src/main/java/io/github/tuguzt/getusdrate/RateCheckService.kt
@@ -1,13 +1,17 @@
 package io.github.tuguzt.getusdrate
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
 import android.util.Log
-import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -24,14 +28,16 @@ class RateCheckService : Service() {
         private const val ARG_TARGET_RATE = "ARG_TARGET_RATE"
 
         fun startService(context: Context, startRate: String, targetRate: String) {
-            context.startService(Intent(context, RateCheckService::class.java).apply {
+            val startIntent = Intent(context, RateCheckService::class.java).apply {
                 putExtra(ARG_START_RATE, startRate)
                 putExtra(ARG_TARGET_RATE, targetRate)
-            })
+            }
+            context.startService(startIntent)
         }
 
         fun stopService(context: Context) {
-            context.stopService(Intent(context, RateCheckService::class.java))
+            val stopIntent = Intent(context, RateCheckService::class.java)
+            context.stopService(stopIntent)
         }
     }
 
@@ -46,7 +52,7 @@ class RateCheckService : Service() {
 
         Log.d(TAG, "rateCheckAttempt = $rateCheckAttempt")
         if (rateCheckAttempt > RATE_CHECK_ATTEMPTS_MAX) {
-            Toast.makeText(applicationContext, "Max attempts count reached, stopping service", Toast.LENGTH_LONG).show()
+            sendNotification()
             stopSelf()
             Log.d(TAG, "Max attempts count reached, stopping service")
             return@Runnable
@@ -65,7 +71,6 @@ class RateCheckService : Service() {
                 targetRate in rateBigDecimal..startRate
                 || (startRate < targetRate && rateBigDecimal >= targetRate)
             ) {
-                Toast.makeText(applicationContext, "Rate = $rate", Toast.LENGTH_LONG).show()
                 sendNotification(rate)
                 stopSelf()
             } else {
@@ -74,9 +79,11 @@ class RateCheckService : Service() {
         }
     }
 
-    override fun onBind(p0: Intent?): IBinder? = null
+    override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createNotificationChannel()
+
         startRate = BigDecimal(intent?.getStringExtra(ARG_START_RATE))
         targetRate = BigDecimal(intent?.getStringExtra(ARG_TARGET_RATE))
 
@@ -92,12 +99,38 @@ class RateCheckService : Service() {
         handler.removeCallbacks(rateCheckRunnable)
     }
 
-    private fun sendNotification(rate: String) {
-        createNotificationChannel()
-        // Write your code here
+    private fun sendNotification(rate: String? = null) {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val flag = when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> PendingIntent.FLAG_IMMUTABLE
+            else -> 0
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, flag)
+
+        val title = when (rate) {
+            null -> "Max attempts count reached, stopping service"
+            else -> "Current USD rate is $rate RUB"
+        }
+        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(title)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .build()
+        startForeground(1, notification)
     }
 
     private fun createNotificationChannel() {
-        // Write your code here
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "My Channel"
+            val channelDescription = "Channel Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance).apply {
+                description = channelDescription
+            }
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
